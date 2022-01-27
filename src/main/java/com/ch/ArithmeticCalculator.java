c113a78497894fb402f27387149b50dd66d5da7f
91863 <91863@LAPTOP-4MFFF399>
2022-01-27 14:08:02 +0530

    Added initial commit

---
+++ src/main/java/com/ch/ArithmeticCalculator.java
@@ -0,0 +1,10 @@
+package com.ch;
+
+public class ArithmeticCalculator {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ src/main/java/com/ch/Multiplication.java
@@ -0,0 +1,6 @@
+package com.ch;
+
+public class Multiplication {
+
+
+}
